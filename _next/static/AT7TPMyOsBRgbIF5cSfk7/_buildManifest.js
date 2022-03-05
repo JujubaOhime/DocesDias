1fa4068f8191433cd363473e0ed6ceeb2d245729
JujubaOhime <45333465+JujubaOhime@users.noreply.github.com>
2022-03-05 03:25:43 +0000

    Deploying to gh-pages from @ JujubaOhime/DocesDias@2dba63d541e3e585e54ef53fee82cabbcbd0cec3 🚀

--- _next/static/AT7TPMyOsBRgbIF5cSfk7/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-dfc9adc3a92df98b.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
